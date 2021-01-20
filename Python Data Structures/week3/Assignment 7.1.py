ea266f9bf781f58dac10e7445701c24f8c18205d
Sena Alzyod <77552412+nutty01@users.noreply.github.com>
2021-01-20 21:41:42 +0200

    Update Assignment 7.1.py

--- Python Data Structures/week3/Assignment 7.1.py
+++ Python Data Structures/week3/Assignment 7.1.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 10 00:10:54 2020
+#Program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case.
 
-@author: snsenel
-"""
 fname = input('Enter file name: ')
 fh = open(fname)
 print(fh.read().upper().strip()) #or for lx in fh: ly=lx.rstrip() print(ly.upper())
